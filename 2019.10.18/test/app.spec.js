c96f4fbeeee31b04d9ee9992a67dbd0222794739
Kira Antonov <antonov128436@gmail.com>
2019-11-08 01:29:38 +0300

    Added pagination function: search(items: Article[], currentPage: number, pageSize: number, filterFn: Function sortFn: Function). Added unit-tests for pagination function.

---
+++ 2019.10.18/test/app.spec.js
@@ -0,0 +1,62 @@
+describe("App", function () {
+    let db = [
+        {
+            "id": 1,
+            "name": "Item1",
+            "content": "Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.",
+            "tags": "com",
+            "createdAt": "2014-12-02T11:10:49Z"
+        },
+        {
+            "id": 2,
+            "name": "Item2",
+            "content": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+            "tags": "net",
+            "createdAt": "2012-05-20T00:15:15Z"
+        },
+        {
+            "id": 3,
+            "name": "Item3",
+            "content": "Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy.",
+            "tags": "com",
+            "createdAt": "2016-06-23T01:05:33Z"
+        },
+        {
+            "id": 4,
+            "name": "Item4",
+            "content": "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
+            "tags": "net",
+            "createdAt": "2015-04-19T18:04:21Z"
+        },
+        {
+            "id": 5,
+            "name": "Item5",
+            "content": "Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
+            "tags": "net",
+            "createdAt": "2018-05-03T04:11:57Z"
+        }
+    ];
+
+    it("should return array with length = 2", function () {
+        expect(search(db, 1, 2, createdAtSort, tagsFilter).length).toBeGreaterThan(0);
+        expect(search(db, 1, 2, createdAtSort, tagsFilter).length).toBeLessThan(3);
+    });
+
+    it("should return not empty array", function () {
+        expect(search(db, 1, 2, createdAtSort, tagsFilter)).toBeInstanceOf(Array);
+        expect(search(db, 1, 2, createdAtSort, tagsFilter).length).toBeGreaterThan(0);
+    });
+
+    it("should return 'Incorrect input data', because items[] is empty", function () {
+        let emptyDb = [];
+        expect(search(emptyDb, 1, 2, createdAtSort, tagsFilter)).toBe("Incorrect input data");
+    });
+
+    it("should return 'Incorrect input data', because pageSize is negative", function () {
+        expect(search(db, 1, -1, createdAtSort, tagsFilter)).toBe("Incorrect input data");
+    });
+
+    it("should return 'Empty result', because resultant array is empty", function () {
+        expect(search(db, 5, 2, createdAtSort, tagsFilter)).toBe("Empty result");
+    });
+});
